60afeb159716b55e90ad8d4d9091a2f18a7e9dfd
Loretix <loretomatinero@gmail.com>
2022-09-29 13:17:43 +0200

    alineamiento v1.2

--- p1_2022/conecta4_2022_arm.s
+++ p1_2022/conecta4_2022_arm.s
@@ -14,6 +14,7 @@
 
 	AREA codigo, CODE
 	EXPORT conecta4_buscar_alineamiento_arm
+	
 
 conecta4_buscar_alineamiento_arm
 	STMDB R13!, {R4-R10,R14}
@@ -25,30 +26,31 @@ conecta4_buscar_alineamiento_arm
 	; estan dentro del tablero 
 	cmp r5, #1
 	blt termina 			; salta si r5 >= 1 
-	cmp r5, #NUM_FILAS
+	cmp r5, #6
 	bgt termina 			; salta si r4 > NUM_FILAS
 	cmp r6, #1
 	blt termina 			; salta si r6 >= 1 
-	cmp r6, #NUM_COLUMNAS 	; salta si r6 <= NUM_COLUMNAS 
+	cmp r6, #7			 	; salta si r6 <= NUM_COLUMNAS 
 	bgt termina 		
 	; comprobamos que la celda no sea vacia, y sea del mismo color 
 	add r10, r4, r5, LSL #3			; r10 = @tablero + 8*fila 
 	ldrb r9, [r10, r6] 				; r9 = dato de la celda = r10 + columna 
-	tst r9, =0x4					; and lógico que actualiza los flags 
+	tst r9, #0x4					; and lógico que actualiza los flags 
 	beq termina 					; salta si flag z = 1 pq la celda estará vacia 
-	and r10, r9, =0x03				; and logico para encontrar color de la celda
+	and r10, r9, #0x03				; and logico para encontrar color de la celda
 	cmp r10, r7						; comparacion del color obtenido con el guardado en r7
 	bne termina						; salta si no son iguales
 	; obtenemos el valor de delta y lo avanzamos 
-	ldr r9, [sp, #36]		 		; r9 = deltaFila = sp + 12
-	add r5, r5, r9					; r5 = nueva_fila = fila + delta_fila
-	ldr r10, [sp, #40]				; r10 = deltaColumna = sp + 8
-	add r6, r6, r10					; r6 = nueva_columna = columna + delta_columna
+	ldr r9, [sp, #32]		 		; r9 = deltaFila = sp + 12
+	add r1, r5, r9					; r5 = nueva_fila = fila + delta_fila
+	ldr r10, [sp, #36]				; r10 = deltaColumna = sp + 8
+	add r2, r6, r10					; r6 = nueva_columna = columna + delta_columna
 	;llamamos a la función recursiva
 	mov r8, #1						; apilamos en resultado un 1
-	STMDB R13!, {r8, r9, r10}		; apilamos los parámetros y el resultado para la siguiente iteración 
+	STMDB R13!, {r8}
+	STMDB R13!, {r9, r10}		; apilamos los parámetros y el resultado para la siguiente iteración 
 	bl conecta4_buscar_alineamiento_arm
-	ldr r1, sp 						; leemos resultado (1)
+	ldr r1, [sp, #8]				; leemos resultado (1)
 	add r0, r0, r1					; r0 = resultado final = resultado anterior (r0) + resultado de esta invocacion de la subrutina(r1)
 	add sp, sp, #12					; liberamo el esacio de los parámetros apilados 
 termina ; salimos de la subrutina 
