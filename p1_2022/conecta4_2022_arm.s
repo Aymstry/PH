4a695bb63e68f5449b726f2c64329a5fcd3a7229
Loretix <loretomatinero@gmail.com>
2022-10-04 17:18:13 +0200

    paso 6

--- p1_2022/conecta4_2022_arm.s
+++ p1_2022/conecta4_2022_arm.s
@@ -11,7 +11,97 @@ deltas_columna 	DCB 0xFF, 0x00, 0xFF, 0xFF
 	IMPORT conecta4_buscar_alineamiento_c
 	PRESERVE8 {TRUE}
 	ENTRY
-
+	
+	; Parámetros que nos pasan a la función 
+    ; r0 = tablero  
+    ; r1 = fila 
+    ; r2 = columna 
+	; r3 = color 
+conecta4_hay_linea_arm_arm
+	STMDB R13!, {R4-R12, R14}
+	mov r4, #0				; contador i del bucle for 
+	LDR r7, =deltas_fila 	; r7 = @deltas_fila	
+for 
+	mov r8, #0		;contador resultado para alineamiento	
+	; funcion alineamiento
+buc ; actualizamos los deltas 
+	ldrsb r6, [r7]			; r4 = valor deltas_fila
+	ldrsb r5, [r7, #4]		; r5 = valor deltas_columna; r8 = r8 +1
+	; comprobamos si son correctos los valores de la celda que nos proporcionan
+	; estan dentro del tablero 
+	cmp r1, #1
+	blt termina 					; salta si r1 < 1 
+	cmp r1, #6
+	bgt termina 					; salta si r5 > NUM_FILAS
+	cmp r2, #1
+	blt termina 					; salta si r6 < 1 
+	cmp r2, #7			 	
+	bgt termina 					; salta si r6 > NUM_COLUMNAS 
+	; comprobamos que la celda no sea vacia, y sea del mismo color 
+	add r10, r0, r1, LSL #3			; r10 = @tablero + 8*fila 
+	ldrb r9, [r10, r2] 				; r9 = dato de la celda = r10 + columna 
+	tst r9, #0x4					; and lógico que actualiza los flags 
+	beq termina 					; salta si flag z = 1 pq la celda estará vacia 
+	and r10, r9, #0x03				; and logico para encontrar color de la celda
+	cmp r10, r3						; comparacion del color obtenido con el guardado en r7
+	bne termina	
+	add r6, r1, r6					; nueva_fila = fila + delta_fila
+	add r5, r2, r5					; nueva_columna = columna + delta_columna
+	add r8, r8, #1					; incremento resultado
+	b buc
+termina		
+	cmp r8, #4						; salta si r4 >= 4 
+	bge continua  
+buc1	
+	; preparamos los parametros para la siguiente invocación 	
+	ldrsb r6, [r7]			; r4 = valor deltas_fila
+	ldrsb r5, [r7, #4]		; r5 = valor deltas_columna; r8 = r8 +1	
+	mov r11, #-1			; r12 = -1 para actualizar los deltas
+	mul r6, r11, r6			; delta_fila
+	mul r5, r11, r5			; delta_columna
+	mov r11, r1
+	mov r12, r2
+	sub r1, r1, r6			; fila - delta_fila
+	sub r2, r2, r5			; columna - delta_columna
+	; comprobamos si son correctos los valores de la celda que nos proporcionan
+	; estan dentro del tablero 
+	cmp r1, #1
+	blt termina 					; salta si r1 < 1 
+	cmp r1, #6
+	bgt termina 					; salta si r5 > NUM_FILAS
+	cmp r2, #1
+	blt termina 					; salta si r6 < 1 
+	cmp r2, #7			 	
+	bgt termina 					; salta si r6 > NUM_COLUMNAS 
+	; comprobamos que la celda no sea vacia, y sea del mismo color 
+	add r10, r0, r1, LSL #3			; r10 = @tablero + 8*fila 
+	ldrb r9, [r10, r2] 				; r9 = dato de la celda = r10 + columna 
+	tst r9, #0x4					; and lógico que actualiza los flags 
+	beq termina 					; salta si flag z = 1 pq la celda estará vacia 
+	and r10, r9, #0x03				; and logico para encontrar color de la celda
+	cmp r10, r3						; comparacion del color obtenido con el guardado en r7
+	bne termina	
+	add r6, r1, r6					; nueva_fila = fila + delta_fila
+	add r5, r2, r5					; nueva_columna = columna + delta_columna
+	add r8, r8, #1					; incremento resultado
+	b buc1
+	add r7, r7, #1					; r7 = r7 +1
+	mov r1, r11
+	mov r2, r12	
+	cmp r8, #4				; salta si r4 >= 4 
+	bge continua  
+	add r4, r4, #1 			; incrementamos contador 
+	cmp r4, #4				; salta si r4 < 4 
+	blt for 	
+	; salimos del bucle y terminamos la subrutina 	
+continua
+	cmp r8, #4 			; Guardamos el resultado en r0 para devolverlo 
+	movge r0, #1
+	movlt r0, #0
+	LDMIA R13!, {R4-R12,R14}
+	mov pc, r14
+	
+	;-------------------------------------------------------------------------------------------------------------------------------------
 	; Parámetros que nos pasan a la función 
     ; r0 = tablero  
     ; r1 = fila 
@@ -75,6 +165,7 @@ continua
 	LDMIA R13!, {R4-R12,R14}
 	mov pc, r14     
 
+	;-------------------------------------------------------------------------------------------------------------------------------------
 	; Parámetros que nos pasan a la función 
     ; r0 = tablero  
     ; r1 = fila 
